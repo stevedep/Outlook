6c2c91bc6c6461f03cba814a41d5d3829a3307df
Steve de Peijper <steve.depeijper@gmail.com>
2019-04-30 17:44:12 +0200

    Update deselectattendees.vb
    filter on type

--- deselectattendees.vb
+++ deselectattendees.vb
@@ -1,20 +1,23 @@
+
 Sub deselect()
 
 Dim objApp As Outlook.Application
+
     Dim objItem As Object
-    Dim objAttendees As Outlook.Recipients
-     
+    Dim objAttendees As Outlook.Recipients     
+
     Set objApp = CreateObject("Outlook.Application")
     Set objItem = objApp.ActiveInspector.CurrentItem
-    Set objAttendees = objItem.Recipients
-    
- 
+    Set objAttendees = objItem.Recipients  
+
     For x = 1 To objAttendees.Count
     On Error Resume Next
-            objAttendees(x).Sendable = False
-    Next
-     
-     
+            If objAttendees(x).Type = 2 Or objAttendees(x).Type = 3 Then
+                objAttendees(x).Sendable = False
+               '  MsgBox (objAttendees(x).Type)
+            End If
+    Next  
+
     Set objApp = Nothing
     Set objItem = Nothing
     Set objAttendees = Nothing
