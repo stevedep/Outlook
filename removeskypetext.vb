89afe5a8ed539218f5329c354a29a02a08856e46
Steve de Peijper <steve.depeijper@gmail.com>
2019-05-01 15:14:01 +0200

    Update removeskypetext.vb

--- removeskypetext.vb
+++ removeskypetext.vb
@@ -1,4 +1,3 @@
-
 Sub removeskypetext()
 
 Dim objApp As Outlook.Application
@@ -9,16 +8,23 @@ Dim objApp As Outlook.Application
     
     Find = "Trouble"
     rt = objItem.Body
+    MsgBox rt
     st = InStr(rt, Find)
     ' MsgBox st
     Find = "original message"
     eind = InStr(rt, Find)
     'MsgBox eind
-    repl = Mid(rt, st, eind - st + 20)
-    newv = Replace(rt, repl, "")
+    If st > 0 Then
+        repl = Mid(rt, st, eind - st + 20)
+        newv = Replace(rt, repl, "")
+        newv = Replace(newv, "!OC([1033])!]", "")
+        newv = Replace(newv, "... vbCrLf", "")
+        
+        End If
     
     objItem.Body = newv
     
     Set objApp = Nothing
     Set objItem = Nothing
 End Sub
+
