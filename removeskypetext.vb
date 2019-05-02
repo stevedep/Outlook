b988ac8ab303fd81e1b34bfdfb55a705288a7425
Steve de Peijper <steve.depeijper@gmail.com>
2019-05-02 09:54:45 +0200

    Update removeskypetext.vb

--- removeskypetext.vb
+++ removeskypetext.vb
@@ -1,3 +1,4 @@
+
 Sub removeskypetext()
 
 Dim objApp As Outlook.Application
@@ -9,22 +10,36 @@ Dim objApp As Outlook.Application
     Find = "Trouble"
     rt = objItem.Body
     MsgBox rt
-    st = InStr(rt, Find)
-    ' MsgBox st
-    Find = "original message"
-    eind = InStr(rt, Find)
-    'MsgBox eind
-    If st > 0 Then
-        repl = Mid(rt, st, eind - st + 20)
-        newv = Replace(rt, repl, "")
-        newv = Replace(newv, "!OC([1033])!]", "")
-        newv = Replace(newv, "... vbCrLf", "")
-        
-        End If
+    't = Replace(rt, vbCrLf, "")
+    'objItem.Body = "Hi, " & vbCrLf & vbCrLf & "Hope this time slot works." & vbCrLf
     
-    objItem.Body = newv
     
-    Set objApp = Nothing
-    Set objItem = Nothing
-End Sub
+    If 1 = 1 Then
+            st = InStr(rt, Find)
+            ' MsgBox st
+            Find = "original message"
+            eind = InStr(rt, Find)
+            'MsgBox eind
+            If st > 0 Then
+                repl = Mid(rt, st, eind - st + 20)
+                newv = Replace(rt, repl, "")
+                newv = Replace(newv, "!OC([1033])!]", "")
+                stl = InStr(newv, "<")
+                eindl = InStr(newv, ">")
+                Link = Mid(newv, stl + 1, eindl - stl - 1)
+                
+                strLink = Link
+                strLinkText = "Join Skype Meeting here"
+                Set objInsp = objItem.GetInspector
+                Set objDoc = objInsp.WordEditor
+                Set objSel = objDoc.Windows(1).Selection
+                objSel.Text = "Hi," & vbCrLf & vbCrLf
+                'strLink = "http://www.outlookcode.com"
+                
+                objDoc.Hyperlinks.Add objSel.Range, strLink, "", "", strLinkText, ""
+                
+           End If
+    
+           ' objItem.Body = newv
+    End If
 
