fc6fac30785ed09daea769b022ab7365d8bff3ee
Steve de Peijper <steve.depeijper@gmail.com>
2019-05-03 06:46:53 +0200

    met namen

--- removeskypetext.vb
+++ removeskypetext.vb
@@ -1,3 +1,4 @@
+
 Sub removeskypetext()
 
     Dim objApp As Outlook.Application
@@ -5,10 +6,23 @@ Sub removeskypetext()
     
     Set objApp = CreateObject("Outlook.Application")
     Set objItem = objApp.ActiveInspector.CurrentItem
+    Set objAttendees = objItem.Recipients
     
     Find = "Trouble"
     rt = objItem.Body
     
+    For x = 1 To objAttendees.Count
+    If objAttendees(x).Type = olRequired Then
+            naam = objAttendees(x).Name
+            co = InStr(naam, ",")
+            naam2 = Mid(naam, co + 2, Len(naam))
+            sp = InStr(naam2, " ")
+            naam3 = Mid(naam2, 1, sp - 1)
+            If naam3 <> "Steve" Then
+                strNames = strNames & naam3 & ", "
+            End If
+    End If
+    Next
     
     If 1 = 1 Then
             st = InStr(rt, Find)
@@ -27,8 +41,11 @@ Sub removeskypetext()
                 Set objInsp = objItem.GetInspector
                 Set objDoc = objInsp.WordEditor
                 Set objsel = objDoc.Windows(1).Selection
-                objsel.Text = "Hi," & vbCrLf & vbCrLf & "Hope this timeslot works." & vbCrLf & vbCrLf & vbCrLf & vbCrLf & vbCrLf & "Kind regards," & vbCrLf & vbCrLf & "Steve"
-                Set myRange = objDoc.Range(Start:=objsel.End - 24, End:=objsel.End - 23)
+                objsel.Text = "Hi " & strNames & vbCrLf & vbCrLf & "Hope this timeslot works." & vbCrLf & _
+                "----------------------------------------------------------------------------" & vbCrLf & vbCrLf & vbCrLf & _
+                "----------------------------------------------------------------------------" & _
+                vbCrLf & vbCrLf & "Kind regards," & vbCrLf & vbCrLf & "Steve"
+                Set myRange = objDoc.Range(Start:=objsel.End - 100, End:=objsel.End - 99)
                 objsel.Hyperlinks.Add myRange, strLink, "", "", strLinkText, ""
            End If
     End If
