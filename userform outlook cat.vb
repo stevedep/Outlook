b85d89ea5e87ca9845e9579d890f992c0f0e692e
Steve de Peijper <steve.depeijper@gmail.com>
2019-05-04 20:19:38 +0200

    Update userform outlook cat.vb

--- userform outlook cat.vb
+++ userform outlook cat.vb
@@ -3,8 +3,12 @@ Private Sub CommandButton1_Click()
     Dim NewEmail As MailItem
 
     Set obApp = Outlook.Application
-    Set NewEmail = obApp.ActiveInspector.CurrentItem
-    c = NewEmail.Categories
+    'Set NewEmail = obApp.ActiveInspector.CurrentItem
+    'Set NewEmail = obApp.Selection.Item(1)
+    Set myOlExp = Application.ActiveExplorer
+ Set myOlsel = myOlExp.Selection
+    'MsgBox myOlsel.Item(1).Subject
+    c = myOlsel.Item(1).Categories
     'If you want to set a specific category to the new email manually
     'You can use the following line instead to show the Category dialog
     'NewEmail.ShowCategoriesDialog
@@ -15,8 +19,32 @@ Private Sub CommandButton1_Click()
     End If
  Next intCurrentRow
     
-    NewEmail.Categories = c & "; " & stritems
+    myOlsel.Item(1).Categories = c & "; " & stritems
+    myOlsel.Item(1).Save
+    Set obApp = Nothing
+    Set NewEmail = Nothing
+End Sub
 
+Private Sub CommandButton2_Click()
+ Dim myNameSpace As Outlook.NameSpace
+ Dim myInbox As Outlook.Folder
+ Dim myDestFolder As Outlook.Folder
+ Dim myItems As Outlook.Items
+ Dim myItem As Object
+ 
+ Set myNameSpace = Application.GetNamespace("MAPI")
+ Set myInbox = myNameSpace.GetDefaultFolder(olFolderInbox)
+Dim obApp As Object
+    Dim NewEmail As MailItem
+
+    Set obApp = Outlook.Application
+    Set myOlExp = Application.ActiveExplorer
+    Set myOlsel = myOlExp.Selection
+    myOlsel.Item(1).Move myInbox.Folders("Done")
+    
+'    myOlsel.Item(1).Save
+    Set myOlExp = Nothing
+    Set myOlsel = Nothing
     Set obApp = Nothing
     Set NewEmail = Nothing
 End Sub
